9921884cb8cd33e36b1ece15ccc2f4075d742689
Jirka Balhar <balhar.j@gmail.com>
2017-05-27 23:38:35 +0200

    test Vue

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ import os
 app = Flask(__name__, static_url_path='')
 app.config['DEBUG'] = True
 
-@app.route("/")
-def hello():
-    # os.chdir("algorithms/MelodyExtraction_MCDNN")
-    # out = subprocess.check_output(["./extract.sh", "0_my_req_test/jazz4.wav", "SAVE_RESULTS/test-flask.txt"])
-    # print(type(out))
-    # return "<pre>"+out.decode()+"</pre>"
-    return "hello wolrd"
-    # return check_output(["ls","-al"])
+# @app.route("/")
+# def hello():
+#     # os.chdir("algorithms/MelodyExtraction_MCDNN")
+#     # out = subprocess.check_output(["./extract.sh", "0_my_req_test/jazz4.wav", "SAVE_RESULTS/test-flask.txt"])
+#     # print(type(out))
+#     # return "<pre>"+out.decode()+"</pre>"
+#     return "hello wolrd"
+#     # return check_output(["ls","-al"])
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run()
---
+++ static/app.js
@@ -0,0 +1,7 @@
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true,
+    test: "ahoj"
+  }
+})
