d26b46aaa0e346935f0cde0146706a318f7fb51b
Jirka Balhar <balhar.j@gmail.com>
2017-07-06 15:00:04 +0200

    prepare file upload on backend

--- app.py
+++ app.py
@@ -1,38 +1,102 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, redirect, url_for, abort
+import hashlib
 import subprocess
 import os
+import re
+import sys
 import json
-app = Flask(__name__, static_url_path='')
-app.config['DEBUG'] = True
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+UPLOADS_DIR = "./uploads"
 DATASETS_DIR = "./datasets"
 ALGORITHMS_DIR = "./algorithms"
 ESTIMATIONS_DIR = "./estimations"
+ALLOWED_EXTENSIONS = ['wav']
 
-# returns 
+
+app = Flask(__name__, static_url_path='')
+app.config['DEBUG'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOADS_DIR
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            abort(400) # 400 Bad Request
+
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            abort(400) # 400 Bad Request
+
+        # extension whitelist check
+        if not allowed_file(file.filename):
+            abort(415) # 415 Unsupported Media Type
+
+        # sanitize/replace filename
+        filename = hashlib.md5(file.read()).hexdigest() + ".wav"
+        filepath = os.path.join(UPLOADS_DIR, filename)
+        # seek because file.read() moves the stream position to the end
+        file.seek(0)
+        # save file
+        file.save(filepath)
+
+        # check header (file command checks for "RIFF....WAVE" at the beginning of the uploaded file)
+        filetype = subprocess.check_output(["file", "-b", filepath]).decode(sys.stdout.encoding)
+        if not ("WAVE audio" in filetype):
+            os.remove(filepath)
+            abort(415) # 415 Unsupported Media Type
+        
+        # validate wav structure
+        try:
+            fileinfo = subprocess.check_output(["shntool", "info", "-i", "wav", filepath]).decode(sys.stdout.encoding)
+        except subprocess.CalledProcessError as e:
+            os.remove(filepath)
+            abort(415) # 415 Unsupported Media Type
+
+        # structure checks
+        inconsistent_header_check = re.search('^\s*Inconsistent header:\s*yes', fileinfo, re.MULTILINE)
+        file_truncated_check = re.search('^\s*File probably truncated:\s*yes', fileinfo, re.MULTILINE)
+        junk_check = re.search('^\s*Junk appended to file:\s*yes', fileinfo, re.MULTILINE)
+        if inconsistent_header_check or file_truncated_check or junk_check:
+            os.remove(filepath)
+            abort(403) # 403 Forbidden
+
+        return filename
+
+    abort(405)
+
 def getName(filename):
     filename = os.path.basename(filename) # removes the path
     filename = os.path.splitext(filename)[0] # removes the extension
 
     return filename
 
-
 @app.route("/getEstimation/<algorithm>/<dataset>/<path:clip>")
 def getEstimation(algorithm, dataset, clip):
     algorithmScript = algorithm+".sh"
     if not algorithmScript in os.listdir(ALGORITHMS_DIR):
         return "invalid algorithm", 403
-    
-    if not dataset in os.listdir(DATASETS_DIR):
-        return "invalid dataset", 403
 
-    # if not dataset in os.listdir(DATASETS_DIR):
-    #     return "invalid clip path", 403
+    if dataset == "upload":
+        if not clip in os.listdir(UPLOADS_DIR):
+            return "invalid upload", 403
+        inputPath = os.path.join(UPLOADS_DIR, clip)
+    else:
+        if not dataset in os.listdir(DATASETS_DIR):
+            return "invalid dataset", 403
+        inputPath = os.path.join(DATASETS_DIR, dataset, clip)
 
-    # os.chdir("algorithms/MelodyExtraction_MCDNN")
     scriptPath = os.path.join(ALGORITHMS_DIR, algorithmScript)
-    inputPath = os.path.join(DATASETS_DIR, dataset, clip)
 
     outputFilename = algorithm+"-"+dataset+"-"+getName(clip)+".txt"
     outputPath = os.path.join(ESTIMATIONS_DIR, outputFilename)
@@ -41,12 +105,8 @@ def getEstimation(algorithm, dataset, clip):
         command = [scriptPath, inputPath, outputPath]
         print("running:", " ".join(command))
         out = subprocess.check_output(command)
-    # print(type(out))
-    # return "<pre>"+out.decode()+"</pre>"
-    # return "hello wolrd"
-    # return subprocess.check_output(["ls","-al"])
-    return send_from_directory(ESTIMATIONS_DIR, outputFilename)
 
+    return send_from_directory(ESTIMATIONS_DIR, outputFilename)
 
 @app.route('/getAlgorithms')
 def getAlgorithms():
